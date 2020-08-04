41e510dce1ddc23201a6a4129356bdb7bb443a36
andreflp <flpandresilva@gmail.com>
2020-08-04 20:19:27 -0300

    ADD: user and appointments migrations, services and routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,26 +1,28 @@
 import { Router } from 'express'
 import { parseISO } from 'date-fns'
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository'
-import CreateAppointmentsService from '../services/CreateAppointmentsService'
+import CreateAppointmentsService from '../services/CreateAppointmentService'
+import { getCustomRepository } from 'typeorm'
 
 const appointmentsRouter = Router()
-const appointmentsRepository = new AppointmentsRepository()
 
-appointmentsRouter.get('/', (request, response) => {
-	const appointments = appointmentsRepository.all()
+appointmentsRouter.get('/', async (request, response) => {
+	const appointmentsRepository = getCustomRepository(AppointmentsRepository) 
+	const appointments = await appointmentsRepository.find()
 
 	return response.json(appointments)
 })
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
 	try {
-		const { provider, date } = request.body
+		const { provider_id, date } = request.body
 
 		const parsedDate = parseISO(date)
 
-		const createAppointment = new CreateAppointmentsService(appointmentsRepository)
+		const createAppointment = new CreateAppointmentsService()
 
-		const appointment = createAppointment.execute({ date: parsedDate, provider })
+		const appointment = await createAppointment.execute({ date: parsedDate, provider_id })
 
 		return response.json(appointment)
 	} catch (error) {
