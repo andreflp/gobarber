f54737107f5818bc861a24224257c059cea57874
andreflp <flpandresilva@gmail.com>
2020-08-08 22:21:46 -0300

    ADD: global error handling

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import 'reflect-metadata'
 import express from 'express'
+import 'express-async-errors'
 import routes from './routes'
 import uploadConfig from './config/upload'
 
 import './database'
+
+import errorHandling from './middlewares/errorHandling'
 
 const app = express()
 
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
+app.use(errorHandling)
 
 app.listen(3333, () => {
   console.log('🚀 Server running on port 3333!')
